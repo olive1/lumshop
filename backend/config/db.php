a47b2ee6c4d6a69cbe2782fe25cd62daadbaf6f2
olive1 <240333983@qq.com>
2017-06-01 18:43:57 +0800

    yii

---
+++ backend/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ytwo',
+    'username' => 'test1',
+    'password' => 'test1',
+    'charset' => 'utf8',
+    'tablePrefix'=>'yii_'
+];
